1afa1375372b6533c9f8cce1dbd5fbddd47d125f
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 08:35:29 +0900

    fadeIn

--- chapter03/lesson06/js/script.js
+++ chapter03/lesson06/js/script.js
@@ -1,3 +1,20 @@
 $(function(){
+  $("#bg").hide();
+  $("#click").click(function(event) {
+    event.stopPropagation();
+    $("#bg").fadeIn(300);
+  });
+  //$("#ok").click(function() {
+  //  $("#bg").fadeOut(300);
+  //});
 
+  $("#ok").click(function(event) {
+    event.preventDefault();
+  })
+  $(document).on("click", (function() {
+    $("#bg").fadeOut(400);
+  }));
+  //document.addEventListener("click", (function() {
+  //  $("#bg").fadeOut(400);
+  //}));
 });
