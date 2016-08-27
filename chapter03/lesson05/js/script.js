1caec48df2912d799bf30f0afbe807b816cb1507
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 08:34:57 +0900

    toggle

--- chapter03/lesson05/js/script.js
+++ chapter03/lesson05/js/script.js
@@ -1,3 +1,12 @@
 $(function(){
+  //document.addEventListener("click", function() {
+  //    $("ul").slideToggle();
+  //  }, false);
+  $("button").on("click", function() {
+    $("ul").slideToggle();
+  });
 
+  $("button").click(function() {
+    $("ul").slideToggle(30000);
+  });
 });
