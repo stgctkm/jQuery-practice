2da2ae1ab37ce2e23c52918041761b133c47cf67
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 08:49:35 +0900

    tab(removeClass,addClass,hide,show)

--- chapter03/lesson08/js/script.js
+++ chapter03/lesson08/js/script.js
@@ -1,3 +1,19 @@
 $(function(){
+  $("#contents div[id != 'tab1']").hide();
+
+  $("a").click(function(){
+    console.log($(this).attr("href"));
+
+    $("#contents div").hide();
+
+    $($(this).attr("href")).show();
+
+    $(".current").removeClass("current");
+
+    $(this).addClass("current");
+
+    return false;
+  });
+
 
 });
