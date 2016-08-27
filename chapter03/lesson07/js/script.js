ad6f4873ab7371dbcb861685aec6557959a71bf2
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 08:35:48 +0900

    attr

--- chapter03/lesson07/js/script.js
+++ chapter03/lesson07/js/script.js
@@ -1,3 +1,9 @@
 $(function(){
-
+  $("a").click(function(){
+    //console.log(this.href);
+    console.log($(this).attr("href"));
+    //$("img").src.val(this.href);
+    $("figure img").attr("src", $(this).attr("href"));
+    return false;
+  }) ;
 });
