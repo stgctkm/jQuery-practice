3452b5c721fbafcb95b77fd0dd7ad0f402116e85
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 13:32:16 +0900

    parallax(animate, removeClass, addClass)

--- chapter05/lesson16_parallax/js/script.js
+++ chapter05/lesson16_parallax/js/script.js
@@ -1,3 +1,16 @@
 $(function() {
+  $("ul li a").click(function() {
+    var page = $(this).html();
 
+    $("#sections").animate({left:(page-1) * -700}, 300);
+
+
+    $("body").css("background-position", (page - 1) * -20 + "px 100%");
+    $("#bg1").css("background-position", (page - 1) * -150 + "px 100%");
+    $("#bg2").css("background-position", (page - 1) * -700 + "px 100%");
+
+    $(".current").removeClass("current");
+    $(this).addClass("current")
+    return false;
+  });
 });
