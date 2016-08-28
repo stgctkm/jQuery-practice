81f920dd6bc15c0ca10454f13e2eb38fd7426870
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 20:47:58 +0900

    slider fadeIn fadeOut

--- chapter07/lesson28_slider-fade-in-out/js/script.js
+++ chapter07/lesson28_slider-fade-in-out/js/script.js
@@ -1,3 +1,37 @@
 $(function(){
+  var count = $("#slide li").length;
+  var current = 1;
+  var next = 2;
+  var interval = 3000;
+  var duration = 500;
+  var timer;
+
+  $("#slide li:not(:first-child)").hide();
+
+  var slideTimer = function() {
+    $("#slide li:nth-child(" + current + ")").fadeOut(duration);
+    $("#slide li:nth-child(" + next + ")").fadeIn(duration);
+    current = next;
+    next = ++next;
+    if (next > count) {
+      next = 1;
+    }
+		$("#button li a").removeClass("target");
+		$("#button li:nth-child("+ current +") a").addClass("target");
+  };
+
+  timer = setInterval(slideTimer, interval);
+
+  $("#button li a").click(function() {
+    next = $(this).html();
+    clearInterval(timer);
+    timer = setInterval(slideTimer, interval);
+
+    slideTimer();
+    return false;
+
+  });
 
 });
+
+//絶対配置でが画像を重ねておく
