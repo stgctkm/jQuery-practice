976b410703fb027b8ff610410efa18c1b7b8ce99
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 18:23:44 +0900

    sticky header

--- chapter06/lesson25_fix-header/js/script.js
+++ chapter06/lesson25_fix-header/js/script.js
@@ -1,3 +1,12 @@
 $(function(){
+  var navPosition = $("nav").offset().top;
 
+  $(window).scroll(function() {
+    // console.log($(window).scrollTop());
+    if ($(this).scrollTop() > navPosition) {
+      $("nav").css("position", "fixed");
+    } else {
+      $("nav").css("position", "static");
+    }
+  });
 });
