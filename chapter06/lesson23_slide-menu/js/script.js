df1a58573fe8569b8ef7e7e916517005adae88fc
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 18:06:07 +0900

    slide menu

--- chapter06/lesson23_slide-menu/js/script.js
+++ chapter06/lesson23_slide-menu/js/script.js
@@ -1,3 +1,13 @@
 $(function(){
+  var direction  = 1;
+  $("button").click(function() {
+    $("body").children().animate({"margin-left": "+=" + direction *250 +"px"}, 200);
+    direction *= -1;
+    // $("body").children().animate({"margin-left": "+=250px"}, 200);
+
+    // $("nav").animate({"margin-left": "0px"}, 200);
+    // $("main").animate({"margin-left": "250px"}, 500);
+    // $("header").animate({"margin-left": "250px"}, 1500);
+  });
 
 });
