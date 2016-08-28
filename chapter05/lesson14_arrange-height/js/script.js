bde505cb6335d7c6f772b26fa648fc7e1fe26410
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 12:54:24 +0900

    arrange height(each)

--- chapter05/lesson14_arrange-height/js/script.js
+++ chapter05/lesson14_arrange-height/js/script.js
@@ -1,3 +1,24 @@
 $(function(){
+  var highest = 0;
+  $("main div").each(function() {
+    if ($(this).height() > highest) {
+      highest = $(this).height();
+    }
+  });
+
+  // $("main div").css("height", highest + "px");
+
+// underscore.js
+  var _high = _.each($("main div"), function(v, k, l) {
+    console.log(k);
+    console.log(v.clientHeight);
+    console.log(l);
+    console.log($(this).attr("height"));
+  })
+  var _high = _.max($("main div"), function(div) {
+    return div.clientHeight;
+  });
+
+  $("main div").css("height", _high.clientHeight + "px");
 
 });
