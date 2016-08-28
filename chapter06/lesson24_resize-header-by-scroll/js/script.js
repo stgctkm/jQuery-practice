b449a16d38a0601ab4ff5f30722c90d155d6c62c
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 18:12:36 +0900

    resize header

--- chapter06/lesson24_resize-header-by-scroll/js/script.js
+++ chapter06/lesson24_resize-header-by-scroll/js/script.js
@@ -1,3 +1,10 @@
 $(function(){
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 300) {
+      $("header").addClass("small");
+    } else {
+      $("header").removeClass("small");
+    }
+  });
 
 });
