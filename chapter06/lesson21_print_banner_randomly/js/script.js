0eee1b31c71db42c0fe4b49d9807b91beefdd259
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 17:26:50 +0900

    random banner

--- chapter06/lesson21_print_banner_randomly/js/script.js
+++ chapter06/lesson21_print_banner_randomly/js/script.js
@@ -1,3 +1,7 @@
 $(function(){
+  var banners = ["red", "yellow", "green", "blue", "brown"];
 
+  var number = Math.floor(Math.random() * 5);
+
+  $("aside img").attr("src", "img/" + banners[number] + ".jpg");
 });
