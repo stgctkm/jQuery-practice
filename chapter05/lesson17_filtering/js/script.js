4e2e84a0900961a1fa042f344ef5a181efaf668b
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 13:56:54 +0900

    filter

--- chapter05/lesson17_filtering/js/script.js
+++ chapter05/lesson17_filtering/js/script.js
@@ -1,3 +1,18 @@
 $(function(){
+  $("button").click(function(){
+    var target = $(this).attr("value");
 
+    // $("ul").animate({"opacity": 0}, 300, function() {
+        //  $(this).children("li img").hide();
+      //  });
+    $("li").each(function() {
+       $(this).animate({"opacity": 0}, 300, function() {
+         $(this).hide();
+      if ($(this).hasClass(target) || target == 'all') {
+        $(this).show();
+        $(this).animate({"opacity": 1}, 300);
+      }
+    });
+     });
+  });
 })
