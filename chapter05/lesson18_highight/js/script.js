ec795717d3c065c6d55c5370160e2e22eba5a40f
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 14:05:34 +0900

    highlight

--- chapter05/lesson18_highight/js/script.js
+++ chapter05/lesson18_highight/js/script.js
@@ -1,3 +1,15 @@
 $(function(){
+  $("td").hover(function(){
+    $(this).parent().addClass("target");
 
+    var index = $(this).index();
+    index++;
+    $("td:nth-child(" + index + ")").addClass("target");
+  }, function(){
+    $(".target").removeClass("target");
+    // $(this).parent().removeClass("target");
+    // var index = $(this).index();
+    // index++;
+    // $("td:nth-child(" + index + ")").removeClass("target");
+  })
 });
