ccc3a164011123c70bb7f97f8cce3f762a335c63
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 10:23:54 +0900

    caption(parent, children, animate, hover)

--- chapter04/lesson12_caption/js/script.js
+++ chapter04/lesson12_caption/js/script.js
@@ -1,3 +1,17 @@
 $(function(){
+  $("li").hover(function() {
+    var caption = $(this).children("img").attr("alt");
+    $(this).append("<div><p>" + caption + "</p></div>");
 
+    $(this).children("div").stop().fadeIn();
+
+    $(this).children("div").children("p").stop().animate({top:0}, 300);
+  }, function() {
+    $(this).children("div").stop().fadeOut();
+
+    $(this).children("div").children("p").stop().animate({top:10}, 300, function() {
+      $(this).parent("div").remove();
+    });
+
+  });
 });
