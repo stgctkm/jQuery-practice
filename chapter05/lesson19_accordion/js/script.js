13af1a21917f126d1dcadd00000a306635511531
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 17:05:50 +0900

    rename

---
+++ chapter05/lesson19_accordion/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('dd[id != "acc1"]').css("width", "0px");
+  $("a").click(function() {
+    if ($(this).hasClass("currentBtn")) return;
+    $(".current").animate({"width": "0px"}, 300);
+    $($(this).attr("href")).animate({"width": "800px"}, 300);
+
+    $("dd").removeClass();
+    $($(this).attr("href")).addClass("current");
+
+    $("a").removeClass();
+    $(this).addClass("currentBtn");
+  });
+});
