634cc782e8cb00e1269d4b034b1e0618f8f785a3
setoguchi <setoguchi.takuma@gmail.com>
2016-08-28 18:35:16 +0900

    higlighted menu

--- chapter06/lesson26_highlighted-menu/js/script.js
+++ chapter06/lesson26_highlighted-menu/js/script.js
@@ -1,3 +1,10 @@
 $(function(){
-
+  $(window).scroll(function() {
+    for (var i = 1; i <= 4; i++) {
+      if ($("section:nth-child(" + i + ")").offset().top < $(this).scrollTop() + 300) {
+        $("nav li").removeClass("current");
+        $("nav li:nth-child(" + i + ")").addClass("current");
+      }
+    }
+  });
 });
